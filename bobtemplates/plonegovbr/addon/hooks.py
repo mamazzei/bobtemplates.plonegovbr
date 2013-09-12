e0266d8365180a967e46fdf6660034a7cadd4ea8
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2013-09-12 18:09:19 -0300

    Undo merge

--- bobtemplates/plonegovbr/addon/hooks.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-
-def split_items_by_pipe(configurator, question, answer):
-    import ipdb
-    ipdb.set_trace()
-    configurator.variables['author.fullname'] =
-        configurator.variables['author.firstname'] + ' ' +
-        answer
-    return answer
